9c0d000f61c7afa83c467682d6aa36fd45d647a1
Ryan <bapezryan84@bellevuecollege.edu>
2017-09-30 18:41:09 -0700

    PBI's 4 and 5 complete

--- scrumsquad/scrumsquad/Controllers/ProductsController.cs
+++ scrumsquad/scrumsquad/Controllers/ProductsController.cs
@@ -10,14 +10,12 @@ namespace scrumsquad.Controllers
 {
     public class ProductsController : ApiController
     {
-        Product[] products = new Product[3];
-
-        public ProductsController()
-        {
-            products[0] = new Product(1, "Tomato Soup", "Groceries", 1);
-            products[1] = new Product(2, "Yo-yo", "Toys", 3.75M);
-            products[2] = new Product(3, "Hammer", "Hardware", 16.99M);
-        }
+        Product[] products = new Product[]
+         {
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+            new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+         };
 
         public IEnumerable<Product> GetAllProducts()
         {
@@ -26,36 +24,13 @@ namespace scrumsquad.Controllers
 
         public IHttpActionResult GetProduct(int id)
         {
-            var product = products.FirstOrDefault((p) => p.id == id);
+            var product = products.FirstOrDefault((p) => p.Id == id);
             if (product == null)
-            { return NotFound(); }
+            {
+                return NotFound();
+            }
             return Ok(product);
         }
-        // GET api/<controller>
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
-        }
+       
     }
 }
--- scrumsquad/scrumsquad/Models/Product.cs
+++ scrumsquad/scrumsquad/Models/Product.cs
@@ -5,21 +5,12 @@ using System.Web;
 
 namespace scrumsquad.Models
 {
-    public class Product
+     public class Product
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string category { get; set; }
-        public decimal price { get; set; }
-
-        public Product(int id, string name, string category, decimal price)
-        {
-            this.id = id;
-            this.name = name;
-            this.category = category;
-            this.price = price;
-        }   
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
     }
 
 }
