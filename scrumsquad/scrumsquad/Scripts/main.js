d70e9fad13f4674bd7f8f9b3973f3af6af3052c7
Ryan <bapezryan84@bellevuecollege.edu>
2017-10-15 14:24:06 -0700

    adds and edits

--- scrumsquad/scrumsquad/Scripts/main.js
+++ scrumsquad/scrumsquad/Scripts/main.js
@@ -1,11 +1,11 @@
 let focusNote = null;
+let notesList;
 
 $(document).on('pagebeforeshow ', '#main', function () {   // see: https://stackoverflow.com/questions/14468659/jquery-mobile-document-ready-vs-page-events
-
+    focusNote = null;
     const template_note = $(".templates .note");
     const el_Display = $("#notes_display");
 
-    let notesList;
     apiGetNotes((err, result) => {
         if (!err) {
             notesList = result;
@@ -41,7 +41,10 @@ $(document).on('pagebeforeshow ', '#main', function () {   // see: https://stack
 
     function editNote(note) {
         focusNote = note;
+        console.log('edit note called');
+        console.log(focusNote);
         $.mobile.changePage($("#add"))
+
     }
 
     function deleteNote(note) {
@@ -56,13 +59,65 @@ $(document).on('pagebeforeshow ', '#main', function () {   // see: https://stack
 });
 
 $(document).on('pagebeforeshow ', '#add', function () {   // see: https://stackoverflow.com/questions/14468659/jquery-mobile-document-ready-vs-page-events
+    let pressCount = 0;
+    let in_Button_Add_Note = $("#in_button_add_note");
+    let in_Text_Subject = $("#in_text_subject");
+    let in_Text_Details = $("#in_text_details");
+    let in_Number_Priority = $("#in_number_priority");   
 
-    // Copy the form I made from the old index over here.
-    // If this is loaded and focusNote is not null, fill the form with the note data
-    // The difference between add and edit is that note.Id will not be null
-    // On submit, make sure to $.mobile.changePage($("#main"))
-    // use apiAddNote(note, callback) and apiEditNote(note, callback) this can be found in api.js
+    if (focusNote != null) {         
+        in_Text_Subject.val(focusNote.Subject);
+        in_Text_Details.val(focusNote.Details);
+        in_Number_Priority.val(focusNote.Priority);              
+    }
+    else {       
+        in_Text_Subject.val(null);
+        in_Text_Details.val(null);
+        in_Number_Priority.val(0);
+    }
 
+    in_Button_Add_Note.click(function () {
+        if (pressCount == 0 && focusNote != null) {
+            pressCount = 1;
+            apiEditNote({
+                Id: focusNote.Id,
+                Subject: in_Text_Subject.val(),
+                Details: in_Text_Details.val(),
+                Priority: in_Number_Priority.val()
+            }, (err, result) => {
+                if (!err) {                    
+                    console.log('successfully edited note');                    
+                    console.log(result);
+                    in_Text_Subject.val(null);
+                    in_Text_Details.val(null);
+                    in_Number_Priority.val(0); 
+                    $.mobile.changePage($("#main"));                   
+                    
+                } else {
+                    console.error(err);
+                }
+                });
+                     
+        }
+        else if (pressCount == 0 && focusNote == null){
+            pressCount = 1;
+            apiAddNote({
+                Subject: in_Text_Subject.val(),
+                Details: in_Text_Details.val(),
+                Priority: in_Number_Priority.val()
+            }, (err, result) => {
+                if (!err) {                    
+                    // console.log(result);
+                    in_Text_Subject.val(null);
+                    in_Text_Details.val(null);
+                    in_Number_Priority.val(0);     
+                    $.mobile.changePage($("#main"));                                     
+                } else {
+                    console.error(err);
+                }
+                });                     
+        }
+    });
 });
 
 
